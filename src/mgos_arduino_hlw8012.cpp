362abe84be8334eaa9fbf83e677ea405088da068
dezzarjay <dezzarjay@hotmail.com>
2018-04-29 17:19:23 +1000

    ok

--- src/mgos_arduino_hlw8012.cpp
+++ src/mgos_arduino_hlw8012.cpp
@@ -13,10 +13,10 @@ void mgos_hlw8012_begin(HLW8012 *hlw, char cf_pin, char cf1_pin, char sel_pin, c
 }
 
 // potential error area
-void mgos_hlw8012_setMode(HLW8012 *hlw, char mode) {
+/*void mgos_hlw8012_setMode(HLW8012 *hlw, char mode) {
   if (hlw == nullptr) return;
   hlw->setMode(mode);
-}
+}*/
 
 double mgos_hlw8012_getCurrent(HLW8012 *hlw) {
      return (hlw == nullptr) ? -1 : hlw->getCurrent();

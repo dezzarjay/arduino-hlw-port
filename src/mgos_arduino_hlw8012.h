7191f40acc01c5ae0e5cb3fe1e1ac1a828cf2019
dezzarjay <dezzarjay@hotmail.com>
2018-04-29 17:17:50 +1000

    ok

--- src/mgos_arduino_hlw8012.h
+++ src/mgos_arduino_hlw8012.h
@@ -19,7 +19,7 @@ void mgos_hlw8012_cf1_interrupt(HLW8012 *hlw);
 
 void mgos_hlw8012_begin(HLW8012 *hlw, char cf_pin, char cf1_pin, char sel_pin, char currentWhen, bool use_interrupts, double pulse_timeout);
 
-void mgos_hlw8012_setMode(HLW8012 *hlw, char mode);
+//void mgos_hlw8012_setMode(HLW8012 *hlw, char mode);
 
 double mgos_hlw8012_getCurrent(HLW8012 *hlw);
 
